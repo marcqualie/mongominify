e277a0c9b192bed4cf8b85456be8e737dc1e9d76
Marc Qualie <marc@marcqualie.com>
2013-08-02 19:21:11 +0100

    Added incomplete test for numeric key storage [ci skip]

--- tests/MongoMinify/Tests/InsertTest.php
+++ tests/MongoMinify/Tests/InsertTest.php
@@ -1,7 +1,7 @@
 <?php
 
 class InsertTest extends MongoMinifyTest {
-	
+
 
 	/**
 	 * Test saving a document to the database
@@ -94,6 +94,56 @@ class InsertTest extends MongoMinifyTest {
 		$document_native = $collection->native->findOne(array('_id' => $document['_id']));
 		$this->assertArrayHasKey('u', $document_native);
 		$this->assertArrayHasKey('e', $document_native);
+
+	}
+
+
+	/**
+	 * Test Numeric Key Insert
+	 */
+	public function testNumericKeyInsert()
+	{
+
+		$this->markTestIncomplete('There is an issue with storing numerical keys correctly comrpessed');
+
+		// Create a collection
+		$collection = $this->getTestCollection();
+
+		// Insert Document
+		$document = array(
+			'_id' => 1,
+			'tags' => array(
+				3 => array(
+					'slug' => 'test',
+					'name' => 'Test'
+				),
+				5 => array(
+					'slug' => 'stuff',
+					'name' => 'Stuff'
+				)
+			)
+		);
+		$collection->insert($document);
+
+		// Read document back natively
+		$document_native = $collection->native->findOne();
+		$document_lookup = $collection->findOne();
+		$this->assertEquals($document, $document_lookup);
+
+		// Make sure the data is stored correctly internally
+		$this->assertEquals(array(
+			'_id' => 1,
+			't' => array(
+				3 => array(
+					's' => 'test',
+					'n' => 'Test'
+				),
+				5 => array(
+					's' => 'stuff',
+					'n' => 'Stuff'
+				)
+			)
+		), $document_native, 'Document not stored correctly in native format');
 
 	}
 
